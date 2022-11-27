c687423fa31710abb250aa4b6977a69d02385770
Rashed Shrahili <rashedshrahili@Rasheds-MacBook-Air.local>
2022-11-27 09:38:12 +0300

    Rashed Sharahili

---
+++ TuwaiqApp/app/src/main/java/com/example/tuwaiqapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tuwaiqapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
